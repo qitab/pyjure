d224969d0775e1f35679fcdecb15f5ea96972ec7
Francois-Rene Rideau <tunes@google.com>
2015-07-18 10:48:08 +0200

    Update dependencies to use Clojure 1.7.0, etc.

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/"}
   :main pyjure.core
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [leijure/delta-position "0.1.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [leijure/delta-position "0.1.0.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
 
                  ;;; TODO: actually use these
@@ -13,7 +13,6 @@
 
                  ;;; Not using these, but experimenting...
                  [org.clojure/tools.trace "0.7.8"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/core.match "0.2.2-SNAPSHOT"]
                  [org.clojure/tools.analyzer "0.3.0"]
                  [org.clojure/tools.analyzer.jvm "0.3.0"]
@@ -31,7 +30,7 @@
   :profiles {:uberjar {:aot :all}
              :dev {:resource-paths ["resources/test"]
                    :dependencies [[me.raynes/conch "0.8.0"]
-                                  [cider/cider-nrepl "0.7.0"]]
+                                  [cider/cider-nrepl "0.9.0"]]
                    :repl-options {:nrepl-middleware
                                   [cider.nrepl.middleware.classpath/wrap-classpath
                                    ;;cider.nrepl.middleware.complete/wrap-complete ;; messes up nrepl-mode
